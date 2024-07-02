582c2af61959187df1cbbd9ed804ae8471ab5e51
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-07-03 00:09:34 +0300

    Remove vite-tsconfig-path
    It doesn't work during building with rollup for some reason. But works
    when the alias specified manually

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            '#': path.resolve(__dirname, './src'),
+        },
+    },
     server: {
         port: 3000,
     },
-    plugins: [react(), svgr(), tsconfigPaths()],
+    plugins: [react(), svgr()],
 })
