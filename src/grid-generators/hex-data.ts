dd1df4bd95dc0e34ee4a850710fd02a9e0d31587
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-07-12 22:32:17 +0300

    honeycombv4

--- src/grid-generators/hex-data.ts
+++ src/grid-generators/hex-data.ts
@@ -9,15 +9,22 @@ import { getNoiseFn } from './noise'
 
 import { getColorPickerFn, setFillColor, setFillColorFromImg } from './fill'
 
-type HexSetArgs = {
-    q: number
-    r: number
-    s: number
+const sparseRect: (w: number, h: number, sparse: number) => Honeycomb.Traverser<Honeycomb.Hex> = (
+    w,
+    h,
+    sparse,
+) => {
+    const rectTraverser = Honeycomb.rectangle({ width: w, height: h })
+    return (hex) => {
+        const res = rectTraverser(hex)
+        if (sparse === 1) {
+            return res
+        }
+        return res.map((h) => {
+            return h.clone({ q: h.q * sparse, r: h.r * sparse, s: h.s * sparse })
+        })
+    }
 }
-
-type OnCreateCallBack = Honeycomb.onCreateCallback<
-    Honeycomb.Hex<{ set: (args: HexSetArgs) => void }>
->
 
 export function genHexData(state: CanvasState, imgData?: Uint8ClampedArray | null): PolygonData {
     const { orientation } = state.cell
@@ -28,24 +35,18 @@ export function genHexData(state: CanvasState, imgData?: Uint8ClampedArray | nul
     const [noises, random] = getNoises(String(state.noise.seed))
     const sizes = getHexCellSize(state.cell, state.canvasSize, state.grid)
 
-    const Hex = Honeycomb.extendHex({ size: sizes.hexSize, orientation })
-    const Grid = Honeycomb.defineGrid(Hex)
-
-    const onCreate: OnCreateCallBack = (hex) => {
-        hex.set({ q: hex.q * sparse, r: hex.r * sparse, s: hex.s * sparse })
-    }
-
-    const grid = Grid.rectangle({
-        width: sizes.cellsNumW,
-        height: sizes.cellsNumH,
-        start: [-1, -1],
-        onCreate: sparse !== 1 ? onCreate : undefined,
+    const Tile = Honeycomb.defineHex({
+        dimensions: { xRadius: sizes.hexSize, yRadius: sizes.hexSize },
+        orientation:
+            orientation == 'pointy' ? Honeycomb.Orientation.POINTY : Honeycomb.Orientation.FLAT,
     })
+
+    const grid = new Honeycomb.Grid(Tile, sparseRect(sizes.cellsNumW, sizes.cellsNumH, sparse))
 
     const palette = state.colors.palette.colors
 
-    const vertices = new Float32Array(grid.length * 6 * 2)
-    const fillColors = new Float32Array(grid.length * 4)
+    const vertices = new Float32Array(grid.size * 6 * 2)
+    const fillColors = new Float32Array(grid.size * 4)
     const result: PolygonData = { vertices, fillColors, type: 'hexagons' }
 
     const noiseFn = getNoiseFn(noises, baseNoise)
@@ -60,10 +61,11 @@ export function genHexData(state: CanvasState, imgData?: Uint8ClampedArray | nul
         return clamp(noiseFn(x, y, sizes.normalW, sizes.normalH) + getRandomNoiseVal(), -1, 1)
     }
 
-    grid.forEach((hexagon, idx) => {
+    let idx = 0
+    grid.forEach((hexagon) => {
         if (isImg) {
-            const col = clamp(Math.ceil(hexagon.x), 0, sizes.cellsNumW)
-            const row = clamp(Math.ceil(hexagon.y), 0, sizes.cellsNumH)
+            const col = clamp(Math.ceil(hexagon.col), 0, sizes.cellsNumW)
+            const row = clamp(Math.ceil(hexagon.row), 0, sizes.cellsNumH)
 
             setFillColorFromImg(
                 row * sizes.cellsNumW + col,
@@ -75,18 +77,17 @@ export function genHexData(state: CanvasState, imgData?: Uint8ClampedArray | nul
             )
         } else {
             const [cx, cy] = [
-                (hexagon.x - (sparse * sizes.cellsNumW) / 2 + state.noise.offsetX + 1) / zoom,
-                (hexagon.y - (sparse * sizes.cellsNumH) / 2 + state.noise.offsetY + 1) / zoom,
+                (hexagon.col - (sparse * sizes.cellsNumW) / 2 + state.noise.offsetX + 1) / zoom,
+                (hexagon.row - (sparse * sizes.cellsNumH) / 2 + state.noise.offsetY + 1) / zoom,
             ]
             setFillColor(getNoiseVal(cx, cy), state.noise, getColor, fillColors, idx)
         }
 
-        const point = hexagon.toPoint()
-        hexagon.corners().forEach((corner, cornerIdx) => {
-            const { x, y } = corner.add(point)
-            vertices[idx * 12 + cornerIdx * 2] = x
-            vertices[idx * 12 + cornerIdx * 2 + 1] = y
+        hexagon.corners.forEach((corner, cornerIdx) => {
+            vertices[idx * 12 + cornerIdx * 2] = corner.x
+            vertices[idx * 12 + cornerIdx * 2 + 1] = corner.y
         })
+        idx++
     })
     return result
 }
